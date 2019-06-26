996122279f6d03ae059ad086525696298ae7535c
hollocaine <hollocaine@github.com>
2019-06-26 22:49:54 +0100

    Update added router

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.json({ msg: 'Welcome here to the API' }));
+
+//Define our routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
