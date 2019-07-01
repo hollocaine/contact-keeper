9489e505477f8270a83929c9ea1e9626a6cc0e4c
hollocaine <hollocaine@github.com>
2019-07-01 22:49:24 +0100

    Error status update

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,7 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (!user) {
-        return res.statue(400).json({ msg: 'Invalid credentials' });
+        return res.status(400).json({ msg: 'Invalid credentials' });
       }
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
