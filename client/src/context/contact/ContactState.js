7d665cda6b8118f69b2bf0a9df48b23c5b0d8d3c
Alan Flynn <alanflynnn@gmail.com>
2019-07-07 22:12:32 +0100

    Update show contact list

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,60 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Alan flynn',
+        email: 'alan@gmail.com',
+        phone: '4564654',
+        type: 'professional'
+      },
+      {
+        id: 2,
+        name: 'Peter Ben',
+        email: 'pb@gmail.com',
+        phone: '648946',
+        type: 'professional'
+      },
+      {
+        id: 3,
+        name: 'Jane Dope',
+        email: 'jd@gmail.com',
+        phone: '123456',
+        type: 'amateur'
+      }
+    ]
+  };
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+  //Add contact
+  //Delete contact
+  //Set current contact
+  //Clear Current contact
+  //Update contact
+  //Filter contacts
+  //Clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+export default ContactState;
