eb22c0b93bb4b2164cf6bce6d79077d85ea17f95
hollocaine <hollocaine@github.com>
2019-07-15 21:12:06 +0100

    Going to deploy

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className="my-1">This a full stack react app for keeping contacts</p>
+      <div className="bg-dark p">
+        <strong>Version: 1.0</strong>
+      </div>
+    </div>
+  );
+};
+
+export default About;
