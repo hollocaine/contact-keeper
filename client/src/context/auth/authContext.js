50b42639c76d306124b2428e7b566779850fd02d
hollocaine <hollocaine@github.com>
2019-07-15 21:58:31 +0100

    Update changes

--- client/src/context/auth/authContext.js
+++ client/src/context/auth/authContext.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import { createContext } from 'react';
 
 const authContext = createContext();
 
 export default authContext;
-=======
-import { createContext } from 'react';
-
-const authContext = createContext();
-
-export default authContext;
->>>>>>> c3bf2e9760416230d95a8463188e8c9266996bae
